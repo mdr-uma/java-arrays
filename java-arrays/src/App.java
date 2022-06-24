8afac45983859c5c0dd073fa9cb238867b874522
github username <um_2003_8@hotmail.com>
2022-06-24 16:58:54 -0400

    add comment

--- java-arrays/src/App.java
+++ java-arrays/src/App.java
@@ -3,6 +3,7 @@ public class App {
 	public static void main(String[] args) {
 		int number = 10;
 		
+		//initialize array with new keyword and whatever number is inside the square bracket is the number of items that array can hold		
 		int[] numbers = new int[5];
 	}
 }
